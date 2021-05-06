b40f8fe0af256bbe53b79016425414c734b64d4e
AFCMS <afcm.contact@gmail.com>
2021-05-06 11:09:24 +0200

    remove command

--- init.lua
+++ init.lua
@@ -5,12 +5,12 @@ mcl_speedrun = {}
 
 
 
-minetest.register_chatcommand("playtime", {
+--[[minetest.register_chatcommand("playtime", {
   params = "",
   description = "Use it to get your own playtime!",
   func = function(name)
     minetest.chat_send_player(name, "Total: "..SecondsToClock(playtime.get_total_playtime(name)).." Current: "..SecondsToClock(playtime.get_current_playtime(name)))
   end,
-})
+})]]
 
 dofile(modpath.."/time.lua")
