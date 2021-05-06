ccaa03fcb0f12051a2d53eb9c16401e747443a45
AFCMS <afcm.contact@gmail.com>
2021-05-06 11:46:31 +0200

    fix

--- time.lua
+++ time.lua
@@ -12,18 +12,51 @@ end)
 
 minetest.register_on_joinplayer(function(player)
     local name = player:get_player_name()
-    current[name] = os.time()
+    local meta = player:get_meta()
+    current[name] = {
+        start = os.time()
+        playtime = meta:get_int("mcl_speedrun:playtime"),
+        playtime_nether = meta:get_int("mcl_speedrun:playtime_nether"),
+        playtime_end = meta:get_int("mcl_speedrun:playtime_end"),
+        is_nether = meta:get_bool("mcl_speedrun:is_nether"),
+        is_end = meta:get_bool("mcl_speedrun:is_end"),
+    }
+    
+    --os.time()
+end)
+
+minetest.register_on_leaveplayer(function(player)
+    local name = player:get_player_name()
+    local meta = player:get_meta()
+    meta:set_int("mcl_speedrun:playtime", current[name].playtime)
+    meta:set_int("mcl_speedrun:playtime_nether", current[name].playtime_nether)
+    meta:set_int("mcl_speedrun:playtime_end", current[name].playtime_end)
+    meta:set_bool("mcl_speedrun:is_nether", current[name].is_nether)
+    meta:set_bool("mcl_speedrun:is_end", current[name].is_end)
+    current[name] = nil
 end)
 
 
---[[function playtime.get_current_playtime(name)
-  if current[name] then
-    return os.time() - current[name]
-  else return 0
-  end
+function mcl_speedrun.get_current_playtime(name)
+    return current[name].playtime
 end
 
---  Function to get playtime
+minetest.register_globalstep(function(dtime)
+    for _,player in pairs(minetest.get_connected_players()) do
+        local name = player:get_player_name()
+        current[name].playtime = os.time() - current[name].start
+    end
+end)
+
+minetest.register_chatcommand("tt", {
+    params = "",
+    description = "Use it to get your own playtime!",
+    func = function(name)
+        return true, mcl_speedrun.get_current_playtime(name)
+    end,
+})
+
+--[[Function to get playtime
 function playtime.get_total_playtime(name)
   return storage:get_int(name) + playtime.get_current_playtime(name)
 end
